79690de78cfa1aae6feba244fcc41a321889cad3
Peter Acs <peter.acs@panoramicdata.com>
2020-06-23 13:17:56 +0200

    CAEP-395 - Refactor PssService response classes

--- Cisco.Api/Data/Pss/CustomerInventoryDetailPaginatedResponse.cs
+++ Cisco.Api/Data/Pss/CustomerInventoryDetailPaginatedResponse.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace Cisco.Api.Data.Pss
 {
 	[XmlRoot("CustomerInventoryDetailPaginatedResponseOutput", Namespace = "http://www.cisco.com/InventoryService")]
-	public class CustomerInventoryDetailPaginatedResponse
+	public class CustomerInventoryDetailPaginatedResponse : PssServiceResponse
 	{
 		/// <summary>
 		/// Page information
@@ -18,19 +17,5 @@ namespace Cisco.Api.Data.Pss
 		/// </summary>
 		[XmlElement("deviceDetail")]
 		public List<DeviceDetail> DeviceDetails { get; set; } = null!;
-
-		/// <summary>
-		/// Time stamp that indicates when this service call was performed.
-		/// </summary>
-		[XmlElement("responseTimestamp")]
-		public DateTime ResponseTimestamp { get; set; }
-
-		/// <summary>
-		/// The same message format is used by different API’s and is
-		/// where the error information is placed in the
-		/// event of an ERROR; the actual content will vary for each different message.
-		/// </summary>
-		[XmlElement("message")]
-		public Message Message { get; set; } = null!;
 	}
 }
