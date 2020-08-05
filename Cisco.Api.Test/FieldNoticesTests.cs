d1854e28490f41b48a96a6fb7c87819a1c89d7f4
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2020-08-05 17:05:00 +0100

    Fix for when ResponseTimestamp is null

---
+++ Cisco.Api.Test/FieldNoticesTests.cs
@@ -0,0 +1,29 @@
+﻿using Cisco.Api.Data.Pss;
+using System.Threading;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cisco.Api.Test
+{
+	public class FieldNoticesTests : Test
+	{
+		public FieldNoticesTests(ITestOutputHelper iTestOutputHelper) : base(iTestOutputHelper)
+		{
+		}
+
+		[Fact]
+		public async void Get_Succeeds()
+		{
+			var ciscoFieldNoticesResponse =
+				await CiscoClient.Pss.GetFieldNoticesAsync(
+					new FieldNoticesRequest
+					{
+						CustomerId = "140194686",
+						InventoryId = "580328"
+					},
+					CancellationToken.None
+				)
+				.ConfigureAwait(false);
+		}
+	}
+}
