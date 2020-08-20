54bc4f004d94e3470358bf404281b7d1c22e685b
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2020-08-20 21:55:08 +0100

    Simplified config

--- Cisco.Api.Test/TestPortalConfig.cs
+++ Cisco.Api.Test/TestPortalConfig.cs
@@ -5,57 +5,57 @@ using System.IO;
 
 namespace Cisco.Api.Test
 {
-    internal class TestPortalConfig
-    {
-        internal TestPortalConfig(
-            string credentialsName, ILogger logger)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../.."))
-                .AddJsonFile("appsettings.json");
+	internal class TestPortalConfig
+	{
+		internal TestPortalConfig(
+			 string credentialsName, ILogger logger)
+		{
+			var builder = new ConfigurationBuilder()
+				 .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../.."))
+				 .AddJsonFile("appsettings.json");
 
-            var configuration = builder.Build();
+			var configuration = builder.Build();
 
-            var defaultCredentialsName = configuration["DefaultCredentials"];
-            credentialsName = $"{credentialsName ?? defaultCredentialsName}Credentials";
-            var credentialsAppSetting = configuration[$"Credentials:{credentialsName}"];
+			var defaultCredentialsName = configuration["DefaultCredentials"];
+			credentialsName = $"{credentialsName ?? defaultCredentialsName}";
+			var credentialsAppSetting = configuration[$"Credentials:{credentialsName}"];
 
-            if (credentialsAppSetting == null)
-            {
-                throw new Exception($"No credentials found in appsettings.json file for {credentialsName}.");
-            }
+			if (credentialsAppSetting == null)
+			{
+				throw new Exception($"No credentials found in appsettings.json file for {credentialsName}.");
+			}
 
-            var credentials = credentialsAppSetting.Split(';');
-            if (credentials.Length != 2)
-            {
-                throw new Exception($"Expected to find credentials in the form ClientId;ClientSecret.  Found '{credentialsAppSetting}'");
-            }
+			var credentials = credentialsAppSetting.Split(';');
+			if (credentials.Length != 2)
+			{
+				throw new Exception($"Expected to find credentials in the form ClientId;ClientSecret.  Found '{credentialsAppSetting}'");
+			}
 
-            var credentialIndex = -1;
-            CiscoClient = new CiscoClient(new CiscoClientOptions
-            {
-                ClientId = credentials[++credentialIndex],
-                ClientSecret = credentials[++credentialIndex],
-            }, logger);
+			var credentialIndex = -1;
+			CiscoClient = new CiscoClient(new CiscoClientOptions
+			{
+				ClientId = credentials[++credentialIndex],
+				ClientSecret = credentials[++credentialIndex],
+			}, logger);
 
-            TestCustomerId = configuration["TestCustomerId"];
-            TestInventoryId = configuration["TestInventoryId"];
-            TestSoftwareEoxId = configuration["TestSoftwareEoxId"];
-            TestHardwareEoxId = configuration["TestHardwareEoxId"];
-            TestFieldNoticesId1 = configuration["TestFieldNoticesId1"];
-            TestFieldNoticesId2 = configuration["TestFieldNoticesId2"];
-            TestPsirtId1 = configuration["TestPsirtId1"];
-            TestPsirtId2 = configuration["TestPsirtId2"];
-        }
+			TestCustomerId = configuration["TestCustomerId"];
+			TestInventoryId = configuration["TestInventoryId"];
+			TestSoftwareEoxId = configuration["TestSoftwareEoxId"];
+			TestHardwareEoxId = configuration["TestHardwareEoxId"];
+			TestFieldNoticesId1 = configuration["TestFieldNoticesId1"];
+			TestFieldNoticesId2 = configuration["TestFieldNoticesId2"];
+			TestPsirtId1 = configuration["TestPsirtId1"];
+			TestPsirtId2 = configuration["TestPsirtId2"];
+		}
 
-        internal CiscoClient CiscoClient { get; }
-        public string TestCustomerId { get; }
-        public string TestInventoryId { get; }
-        public string TestSoftwareEoxId { get; }
-        public string TestHardwareEoxId { get; }
-        public string TestFieldNoticesId1 { get; }
-        public string TestFieldNoticesId2 { get; }
-        public string TestPsirtId1 { get; }
-        public string TestPsirtId2 { get; }
-    }
+		internal CiscoClient CiscoClient { get; }
+		public string TestCustomerId { get; }
+		public string TestInventoryId { get; }
+		public string TestSoftwareEoxId { get; }
+		public string TestHardwareEoxId { get; }
+		public string TestFieldNoticesId1 { get; }
+		public string TestFieldNoticesId2 { get; }
+		public string TestPsirtId1 { get; }
+		public string TestPsirtId2 { get; }
+	}
 }
