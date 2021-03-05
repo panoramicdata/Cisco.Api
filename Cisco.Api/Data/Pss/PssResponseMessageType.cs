5ad026a90cb789632e772a2ec00bb76f8575c49d
Ryan Halley <ryan.halley@panoramic.systems>
2021-03-05 21:37:47 +0000

    Add pss message response type.

--- Cisco.Api/Data/Pss/PssResponseMessageType.cs
+++ Cisco.Api/Data/Pss/PssResponseMessageType.cs
@@ -3,7 +3,6 @@
     public static class PssResponseMessageType
     {
         public const string Success = "SUCCESS";
-        public const string FailureNoAlertsPresent = "No alerts present for the given input parameters";
-        public const string FailureNoDataAvailable = "No data available for given input parameters.";
+        public const string Failure = "FAILURE";
     }
 }
