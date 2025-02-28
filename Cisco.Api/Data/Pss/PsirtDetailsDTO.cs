370157231a923ba90c1f01410c55c302bd07a290
Ryan Halley <ryan.halley@panoramicdata.com>
2025-02-28 16:59:45 +0000

    Add Caveats to PSS.GetPsirtDetailsAsync()

--- Cisco.Api/Data/Pss/PsirtDetailsDTO.cs
+++ Cisco.Api/Data/Pss/PsirtDetailsDTO.cs
@@ -3,56 +3,62 @@ using System.Xml.Serialization;
 
 namespace Cisco.Api.Data.Pss;
 
+/// <summary>
+/// The ResponseDTO when getting PsirtDetails
+/// </summary>
+public class PsirtDetailsDTO
+{
     /// <summary>
-    /// The ResponseDTO when getting PsirtDetails
+    /// PSIRT id of the PSIRT item.
     /// </summary>
-    public class PsirtDetailsDTO
-    {
-        /// <summary>
-        /// PSIRT id of the PSIRT item.
-        /// </summary>
-        [XmlElement("psirtId")]
-        public string PsirtId { get; set; } = null!;
+    [XmlElement("psirtId")]
+    public string PsirtId { get; set; } = null!;
 
-        /// <summary>
-        /// Type of distribution for the PSIRT.
-        /// </summary>
-        [XmlElement("distributionType")]
-        public string DistributionType { get; set; } = null!;
+    /// <summary>
+    /// Type of distribution for the PSIRT.
+    /// </summary>
+    [XmlElement("distributionType")]
+    public string DistributionType { get; set; } = null!;
 
-        /// <summary>
-        /// Document number for the PSIRT.
-        /// </summary>
-        [XmlElement("documentNumber")]
-        public string DocumentNumber { get; set; } = null!;
+    /// <summary>
+    /// Document number for the PSIRT.
+    /// </summary>
+    [XmlElement("documentNumber")]
+    public string DocumentNumber { get; set; } = null!;
 
-        /// <summary>
-        /// External url for the PSIRT.
-        /// </summary>
-        [XmlElement("externalURL")]
-        public string ExternalURL { get; set; } = null!;
+    /// <summary>
+    /// External url for the PSIRT.
+    /// </summary>
+    [XmlElement("externalURL")]
+    public string ExternalURL { get; set; } = null!;
 
-        /// <summary>
-        /// Date the PSIRT was first published.
-        /// </summary>
-        [XmlElement("firstPublished")]
-        public DateTime FirstPublished { get; set; }
+    /// <summary>
+    /// Date the PSIRT was first published.
+    /// </summary>
+    [XmlElement("firstPublished")]
+    public DateTime FirstPublished { get; set; }
 
-        /// <summary>
-        /// Headline for the PSIRT.
-        /// </summary>
-        [XmlElement("headline")]
-        public string Headline { get; set; } = null!;
+    /// <summary>
+    /// Headline for the PSIRT.
+    /// </summary>
+    [XmlElement("headline")]
+    public string Headline { get; set; } = null!;
 
-        /// <summary>
-        /// Date the PSIRT was last updated.
-        /// </summary>
-        [XmlElement("lastUpdated")]
-        public DateTime LastUpdated { get; set; }
+    /// <summary>
+    /// Date the PSIRT was last updated.
+    /// </summary>
+    [XmlElement("lastUpdated")]
+    public DateTime LastUpdated { get; set; }
 
-        /// <summary>
-        /// Revision number for the PSIRT.
-        /// </summary>
-        [XmlElement("revisionNumber")]
-        public string RevisionNumber { get; set; } = null!;
-    }
+    /// <summary>
+    /// Revision number for the PSIRT.
+    /// </summary>
+    [XmlElement("revisionNumber")]
+    public string RevisionNumber { get; set; } = null!;
+
+	/// <summary>
+	/// Caveats for the PSIRT.
+	/// </summary>
+	[XmlElement("caveats")]
+	public string Caveats { get; set; } = null!;
+}
