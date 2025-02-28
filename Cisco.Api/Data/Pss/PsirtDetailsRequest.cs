84847136169cea7ede63d22344a6b678f634469a
Ryan Halley <ryan.halley@panoramicdata.com>
2025-02-28 17:21:50 +0000

    Formatting.

--- Cisco.Api/Data/Pss/PsirtDetailsRequest.cs
+++ Cisco.Api/Data/Pss/PsirtDetailsRequest.cs
@@ -3,16 +3,16 @@ using System.Xml.Serialization;
 
 namespace Cisco.Api.Data.Pss;
 
+/// <summary>
+/// The PsirtDetails Request
+/// </summary>
+[XmlRoot("PSIRTDetailsRequestInput", Namespace = "http://www.cisco.com/PSIRTAlertService")]
+public class PsirtDetailsRequest
+{
     /// <summary>
-    /// The PsirtDetails Request
+    /// Was obtained from the getPSIRTDetails API service call.
     /// </summary>
-    [XmlRoot("PSIRTDetailsRequestInput", Namespace = "http://www.cisco.com/PSIRTAlertService")]
-    public class PsirtDetailsRequest
-    {
-        /// <summary>
-        /// Was obtained from the getPSIRTDetails API service call.
-        /// </summary>
-        [XmlArray("psirtIds")]
-        [XmlArrayItem("psirtId")]
-        public List<string> Ids { get; set; } = null!;
-    }
+    [XmlArray("psirtIds")]
+    [XmlArrayItem("psirtId")]
+    public List<string> Ids { get; set; } = null!;
+}
--- Cisco.Api/Data/Pss/PsirtDetailsResponse.cs
+++ Cisco.Api/Data/Pss/PsirtDetailsResponse.cs
@@ -3,15 +3,15 @@ using System.Xml.Serialization;
 
 namespace Cisco.Api.Data.Pss;
 
+/// <summary>
+/// The PsirtDetails Response
+/// </summary>
+[XmlRoot("PSIRTDetailsResponseOutput", Namespace = "http://www.cisco.com/PSIRTAlertService")]
+public class PsirtDetailsResponse : PssServiceResponse
+{
     /// <summary>
-    /// The PsirtDetails Response
+    /// The List of PsirtDetails
     /// </summary>
-    [XmlRoot("PSIRTDetailsResponseOutput", Namespace = "http://www.cisco.com/PSIRTAlertService")]
-    public class PsirtDetailsResponse : PssServiceResponse
-    {
-        /// <summary>
-        /// The List of PsirtDetails
-        /// </summary>
-        [XmlElement("PSIRTDetailsList")]
-        public List<PsirtDetailsDTO> Details { get; set; } = new List<PsirtDetailsDTO>();
-    }
+    [XmlElement("PSIRTDetailsList")]
+    public List<PsirtDetailsDTO> Details { get; set; } = new List<PsirtDetailsDTO>();
+}
